fa5c1f76cb8c132d10886a8a5e797027afca2108
Emad Ferdowsi <emad.ferdowsi@outlook.com>
2023-10-15 03:07:44 +0330

    add: user services

--- whatsapp-messaging/services/whatsapp/user/user.go
+++ whatsapp-messaging/services/whatsapp/user/user.go
@@ -1,2 +1,51 @@
 package user
 
+import (
+	"context"
+	"fmt"
+	"whatsapp-messaging/internal/logger"
+	"whatsapp-messaging/internal/repository/dbrepo"
+)
+
+func SetJID(ctx context.Context, jid string) error {
+	db, err := dbrepo.GetGormConn(ctx)
+	if err != nil {
+		return err
+	}
+
+	userId, _, err := ReadUserContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	if err := db.Create(&User{UserID: userId, Jid: jid}).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewUserContext(ctx context.Context, userID int64, jid string) (context.Context, error) {
+	if userID == 0 {
+		err := fmt.Errorf("no user id found")
+		logger.Error(ctx, err)
+		return nil, err
+	}
+
+	ctx = context.WithValue(ctx, "user_id", userID)
+	if jid != "" {
+		ctx = context.WithValue(ctx, "jid", jid)
+	}
+
+	return ctx, nil
+}
+
+func ReadUserContext(ctx context.Context) (userId int64, jid string, err error) {
+	userIdValue := ctx.Value("user_id")
+	if userIdValue == nil {
+		return 0, "", fmt.Errorf("no user id found")
+	}
+	jidValue := ctx.Value("jid")
+
+	return userIdValue.(int64), jidValue.(string), nil
+}
